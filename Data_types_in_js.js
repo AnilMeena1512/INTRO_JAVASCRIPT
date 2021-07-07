abd3919bfd29c05553ce96383f082788ed7985c8
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-07 12:18:38 +0530

    Create Data_types_in_js.js

---
+++ Data_types_in_js.js
@@ -0,0 +1,17 @@
+//data types in js
+//numbers
+var num1=40;
+var num2=60;
+
+//strings
+str1="this is a string"
+str2='this also a string'
+
+//objects
+
+var marks={
+    anil:64,
+    sunil:70,
+    shubham:80
+}
+console.log(marks)
