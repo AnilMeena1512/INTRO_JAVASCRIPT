ee920881a10dec4f50412daa6e7a0eb5bb116117
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-07 16:45:35 +0530

    Create String_methods.js

---
+++ String_methods.js
@@ -0,0 +1,10 @@
+//string methods in JavaScript
+
+myString="Anil is a good boy"
+
+console.log(myString.length)
+console.log(myString.indexOf("Anil"))
+console.log(myString.lastIndexOf("good"))
+console.log(myString.slice(0,3))
+d=myString.replace("Anil","sunil")
+console.log(d,myString)
