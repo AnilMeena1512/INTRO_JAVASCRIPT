6d52e7e96c5f3d01f556e0ecb4337062795c3cb9
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-07 16:29:38 +0530

    Create Array_methods.js

---
+++ Array_methods.js
@@ -0,0 +1,15 @@
+//Array methods in JavaScript
+
+myArr=["fan","camera",34,null,true]
+
+console.log(myArr.length)
+myArr.pop()
+console.log(myArr)
+myArr.push("harry")
+console.log(myArr)
+myArr.shift()
+console.log(myArr)
+myArr.unshift("harry")
+console.log(myArr)
+myArr.toString()
+console.log(myArr)
