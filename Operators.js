9bd7ce5e273631903d5b8cf2564ae97f3312e053
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-07 14:56:14 +0530

    Create Operators.js

---
+++ Operators.js
@@ -0,0 +1,42 @@
+//operators in JavaScript
+//arithmatic operators
+var a= 100
+var b= 10
+console.log("the value of a+b is:",a+b)
+console.log("the value of a-b is:",a-b)
+console.log("the value of a*b is:",a*b)
+console.log("the value of a/b is:",a/b)
+
+//assignment operators
+var c=b
+c+=2
+console.log(c)
+// comparison operators
+var x=20;
+var y=10
+console.log(x==y)
+console.log(x>=y)
+console.log(x<=y)
+console.log(x>y)
+console.log(x<y)
+
+//logical operators
+
+//logical and
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+//logical or
+
+console.log(true || true)
+console.log(true || false)
+console.log(false || true)
+console.log(false || false)
+
+//logical not
+
+console.log(!false)
+console.log(!true)
