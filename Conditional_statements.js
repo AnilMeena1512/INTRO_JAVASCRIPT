5f72a18884b0dac5e1d16bc44346cf110c545872
AnilMeena1512 <72185078+AnilMeena1512@users.noreply.github.com>
2021-07-07 15:40:19 +0530

    Create Conditional_statements.js

---
+++ Conditional_statements.js
@@ -0,0 +1,19 @@
+//conditional statements in JavaScript
+
+var age=15;
+if (age<4){
+    console.log('you are a kid ');
+}
+
+else if(age<8){
+    console.log('still you are a kid');
+}
+else if(age<12){
+    console.log('sorry still you are a kid');
+}
+else if(age<16){
+    console.log('uff! sorry still you are  a kid');
+}
+else{
+    console.log('you are not a kid');
+}
